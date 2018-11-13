e189f4a380be222222b46a29426af7907fb5e5c9
encima <encima@gmail.com>
2018-11-13 15:17:15 +0100

    adding standard data and props so pages load without issue.

--- src/components/ActivityDetailsForm.vue
+++ src/components/ActivityDetailsForm.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="prereq">
-        <div v-bind:class="{'invalid': !userDetails.canton}" class="form-field">
+        <div v-bind:class="{'invalid': !details.canton}" class="form-field">
             <label>Region Ihres Dialekts</label>
             <div class="select-wrapper">
-                <select v-model="userDetails.canton" name="canton" id="canton" placeholder="Region you have spent most of your life">
+                <select v-model="details.canton" name="canton" id="canton"
+                        placeholder="Region you have spent most of your life">
                     <option :key="r.value" v-for="r in regions" :value="r.value">{{r.label}}</option>
                     <template v-if="project.name==='Übersetzen'">
                         <option disabled>--------------</option>
@@ -13,10 +14,10 @@
                 <span class="error" v-if="errors.canton">Ihre Region ist erforderlich</span>
             </div>
         </div>
-        <div v-bind:class="{'invalid': !userDetails.age}" class="form-field">
+        <div v-bind:class="{'invalid': !details.age}" class="form-field">
             <label>Alter</label>
             <div class="select-wrapper">
-                <select v-model="userDetails.age" name="range" id="range" placeholder="Age Range">
+                <select v-model="details.age" name="range" id="range" placeholder="Age Range">
                     <option :key="a.value" v-for="a in ageRange" :value="a.value">{{a.label}}</option>
                 </select>
                 <span class="error" v-if="errors.age">Ihre Altersgruppe ist erforderlich</span>
@@ -27,7 +28,31 @@
 
 <script>
     export default {
-        name: "ActivityDetailsForm"
+      name: 'ActivityDetailsForm',
+      props: {
+        project: {
+          type: Object
+        },
+        regions: {
+          type: Array,
+          default: () => {
+            return []
+          }
+        }
+      },
+      data () {
+        return {
+          details: {
+            age: undefined,
+            canton: undefined
+          },
+          errors: {
+            canton: false,
+            age: false
+          },
+          ageRange: []
+        }
+      }
     }
 </script>
 
