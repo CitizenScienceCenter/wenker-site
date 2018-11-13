3235c0738ab5b879ec6747fe9a53fa475e0d48a6
Marcel Tanner <marcel.tanner@uzh.ch>
2018-11-13 13:52:15 +0100

    Commmented Router functionality

--- src/router/router.js
+++ src/router/router.js
@@ -12,32 +12,32 @@ export const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  const lang = store.state.language;
-  const title = i18n.messages[lang][to.meta.page]['title'];
-  if (title) {
-    document.title = title;
-  } else {
-    document.title = "";
-  }
-
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (store.state.user.currentUser) {
-      store.dispatch('user/validate').then(v => {
-        if (v) {
-          next()
-        } else {
-          store.dispatch('user/generateAnon').then(u => {
-            next()
-          })
-        }
-      })
-    } else {
-      store.dispatch('user/generateAnon').then(u => {
-        next()
-      })
-    }
-  } else {
-    next()
-  }
+  // const lang = store.state.language;
+  // const title = i18n.messages[lang][to.meta.page]['title'];
+  // if (title) {
+  //   document.title = title;
+  // } else {
+  //   document.title = "";
+  // }
+  //
+  // if (to.matched.some(record => record.meta.requiresAuth)) {
+  //   if (store.state.user.currentUser) {
+  //     store.dispatch('user/validate').then(v => {
+  //       if (v) {
+  //         next()
+  //       } else {
+  //         store.dispatch('user/generateAnon').then(u => {
+  //           next()
+  //         })
+  //       }
+  //     })
+  //   } else {
+  //     store.dispatch('user/generateAnon').then(u => {
+  //       next()
+  //     })
+  //   }
+  // } else {
+  //   next()
+  // }
 
 });
