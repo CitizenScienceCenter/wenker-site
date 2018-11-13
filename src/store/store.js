c7fc9dec7093a2f371196beb4cf7b84dd4359d75
Marcel Tanner <marcel.tanner@uzh.ch>
2018-11-13 11:08:07 +0100

    Removing Store modules for API

--- src/store/store.js
+++ src/store/store.js
@@ -1,39 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import api from './modules/api'
-import user from './modules/user'
-import project from './modules/project'
-import task from './modules/task'
-import submission from './modules/submission'
-import media from './modules/media'
-import upload from './modules/upload'
 import settings from './modules/settings'
-import consts from './modules/consts'
-import projectFilter from './modules/components/project-filter'
 import VuexPersist from 'vuex-persist'
 
 
 const vuexPersist = new VuexPersist({
   key: 'my-app',
   storage: localStorage
-})
+});
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
   modules: {
-    user,
-    project,
-    api,
-    consts,
-    task,
-    media,
-    upload,
-    settings,
-    submission,
-    projectFilter
+    settings
   },
   strict: debug,
   plugins: [vuexPersist.plugin]
