f7db5b0a769b705e3d3b35f436a483d347100249
encima <encima@gmail.com>
2018-11-21 14:33:45 +0100

    login and register working

--- src/components/login-form.vue
+++ src/components/login-form.vue
@@ -1,17 +1,17 @@
 <i18n>
     {
-        "de": {
-            "label-email": "Email",
-            "label-password": "Passwort",
-            "button-login": "Anmelden",
-            "button-forgotten": "Passwort vergessen?"
-        },
-        "en": {
-            "label-email": "Email",
-            "label-password": "Password",
-            "button-login": "Anmelden",
-            "button-forgotten": "Passwort vergessen?"
-        }
+    "de": {
+    "label-email": "Email",
+    "label-password": "Passwort",
+    "button-login": "Anmelden",
+    "button-forgotten": "Passwort vergessen?"
+    },
+    "en": {
+    "label-email": "Email",
+    "label-password": "Password",
+    "button-login": "Anmelden",
+    "button-forgotten": "Passwort vergessen?"
+    }
     }
 </i18n>
 
@@ -29,9 +29,9 @@
                 <input v-model="password" type="password" id="password" name="password" autocomplete="password"
                        :disabled="loading"/>
             </div>
-            <span class="error" v-if="error">{{error}}</span>
 
             <button type="submit" class="button button-primary" :disabled="loading">{{ $t('button-login') }}</button>
+            <span class="error" v-if="error">{{error}}</span>
         </form>
         <br>
         <button @click="reset" class="button button-secondary" :disabled="loading">{{ $t('button-forgotten') }}</button>
@@ -39,51 +39,53 @@
 </template>
 
 <script>
-  import _default, { mapState, mapGetters } from 'vuex'
+    import _default, {mapState, mapGetters} from 'vuex'
 
-  export default {
-    name: 'LoginForm',
-    data() {
-      return {
-        msg: 'Please login here',
-        email: '',
-        password: '',
-        username: '',
-        success: false,
-        error: false
-      }
-    },
-    computed: mapState({
-      currentUser: state => state.c3s.user.currentUser,
-      loading: state => state.c3s.user.loading,
-      client: state => state.c3s.api
-    }),
-    watch: {
-      'currentUser'(to, from) {
-        // console.log(to)
-        if (to !== null || to !== undefined) {
-        }
-      }
-    },
-    mounted() {
-      // TODO errors should be in a global state and cleared on load
-      // this.$store.commit('settings/SET_ERROR', null)
-    },
-    methods: {
-      login() {
-        this.$store
-          .dispatch('c3s/user/login', { user: { email: this.email, pwd: this.password } })
-          .then(user => {
-            if (user !== false) {
-              this.$router.push({ name: 'Home' })
+    export default {
+        name: 'LoginForm',
+        data() {
+            return {
+                msg: 'Please login here',
+                email: '',
+                password: '',
+                username: '',
+                success: false,
+                error: false
             }
-          })
-      },
-      reset() {
-        this.$router.push({ name: 'ResetRequest' })
-      }
+        },
+        computed: mapState({
+            currentUser: state => state.c3s.user.currentUser,
+            loading: state => state.c3s.settings.loading,
+        }),
+        watch: {
+            'currentUser'(to, from) {
+                // console.log(to)
+                if (to !== null || to !== undefined) {
+                }
+            }
+        },
+        mounted() {
+            // TODO errors should be in a global state and cleared on load
+            // this.$store.commit('settings/SET_ERROR', null)
+        },
+        methods: {
+            login() {
+                this.error = null;
+                this.$store
+                    .dispatch('c3s/user/login', {user: {email: this.email, pwd: this.password}})
+                    .then(user => {
+                        if (user.status === 200) {
+                            this.$router.push({name: 'Home'})
+                        } else {
+                            this.error = 'Login failed'
+                        }
+                    })
+            },
+            reset() {
+                this.$router.push({name: 'ResetRequest'})
+            }
+        }
     }
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
