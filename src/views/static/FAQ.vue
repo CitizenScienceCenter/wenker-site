544c158c6281a33f6a4103487e4fbf911cde4f53
encima <encima@gmail.com>
2019-06-05 11:00:34 +0200

    faqs updated with texts from ling dept

--- src/views/static/FAQ.vue
+++ src/views/static/FAQ.vue
@@ -2,33 +2,37 @@
   {
   "de": {
   "heading": "Fragen und Antworten",
-  "question-1": "Was tun, wenn man ein Wort/Buchstabe nicht erkennt?",
-  "answer-1": "Wenn ein Wort oder ein Buchstabe nicht entziffert werden kann, wird folgender Platzhalter eingefügt: [...]<br><a href='#transkriptionskonventionen'>siehe Transkriptionskonventionen</a>",
-  "question-2": "Was tun, wenn man ein Sonderzeichen nicht transkribieren kann?",
-  "answer-2": "Schauen Sie in der vorhandenen Sonderzeichentabelle nach. Falls das gewünschte Sonderzeichen nicht vorhanden ist, schreiben Sie eine Mail an <a href='mailto:info@citizenscience.ch'>info@citizenscience.ch</a>.",
-  "question-3": "Ich komme von X, ich möchte den Bogen von Y transkribieren, ist das möglich?",
-  "answer-3": "Grundsätzlich ist das möglich, ja. Sie können so viele Bögen transkribieren, wie Sie möchten. Nur die eigene Übersetzung der 40 Sätze kann nur einmal erfolgen.",
+  "question-1": "Was ist der Vorteil einer Registrierung?",
+  "answer-1": "Registrierte Benutzer haben folgende Vorteile:<ul><li> Sie können Ihre Arbeit auf einem anderen Computer fortsetzen.</li> <li> Sie erhalten Zugriff auf die Bögen, die Sie bereits transkribiert haben (unter Ihrem Profil)</li> <li> Sie können Ihre persönlichen Daten (Region, Altersbereich) speichern, damit Sie sie nicht jedes Mal erneut eingeben müssen.</li></ul>",
+  "question-2": "Wie gehe ich beim Transkribieren vor?",
+  "answer-2": "Zuerst haben Sie 2 Möglichkeiten:<ul><li>Sie können die Bögen in zufälliger Reihenfolge transkribieren.</li><li>Sie können Bögen aus Orten innerhalb eines Kantons transkribieren. </li></ul> <p>Wenn Sie keine Auswahl für Ihre Region treffen, erscheinen die Bögen aus unserer Datenbank in völlig zufälliger Reihenfolge.</p> <p>Der ausgewählte Bogen kann mit der Maus (oder dem Touchscreen) gescrollt werden, und Sie haben auch die Möglichkeit, das Bild zu vergrössern (zoomen), um es deutlicher zu machen.</p> <p>Unterhalb des Bildes befindet sich ein Textfeld, das den ersten Satz zeigt. Sie können zwischen den Sätzen wechseln, indem Sie auf die Schaltfläche 'weiter' klicken oder eine Satznummer aus der Dropdown-Liste auswählen.</p> <p>Wenn Sie einen Bogen ausfüllen, wird alles, was Sie tun, lokal auf Ihrem Rechner gespeichert. Das bedeutet, dass Sie beispielsweise zu Satz 4 wechseln können, weiter zu Satz 5 gehen können und schliesslich trotzdem zu Satz 4 zurückkehren können, um Änderungen vorzunehmen. Sobald Sie Ihre Arbeit abschliessen, d.h. auf den nächsten Bogen wechseln oder Ihre Sitzung beenden, werden diese Transkriptionen in der Datenbank gespeichert und können nicht mehr bearbeitet werden. Jedes Mal, wenn Sie erneut einen Bogen aufrufen (entweder um zu korrigieren oder um ihn zu vervollständigen), wird Ihre Transkription als neu behandelt, wobei das Eingabefeld informiert, welche Sätze Sie bereits bearbeitet haben.</p> <p>Wenn Sie irgendwelche Schwierigkeiten haben, können Sie den Kommentarbereich benutzen, um den Satz zu besprechen, an dem Sie gerade arbeiten. Ihr Kommentar wird automatisch dem Satz zugeordnet. Sie werden dann Hilfe von der Community und den am Projekt Beteiligten erhalten.</p>",
+  "question-3": "Was soll ich mit Sonderzeichen machen?",
+  "answer-3": "<p>Es kann in den Bögen Sonderzeichen geben, die auf Ihrer Tastatur schwer zu finden sind. Einige werden oberhalb des Eingabefelds für die Sätze angezeigt und können angeklickt werden, um sie dem letzten Buchstaben, den Sie geschrieben haben hinzuzufügen. Tippen Sie also zuerst den gewünschten Buchstaben und klicken Sie anschliessend auf das Sonderzeichen, um es mit dem vorherigen Buchstaben zu kombinieren.</p> <p>Wenn Sie beispielsweise das Symbol ā verwenden möchten, geben Sie a ein und klicken Sie anschliessend auf die erste Schaltfläche in der Liste der Sonderzeichen, also auf ē.</p> <p>Es gibt einige Sonderzeichen, die sie direkt an der gewünschten Stelle einfügen können. Es handelt sich um die folgenden Symbole, die sie am Ende der Sonderzeichenreihe finden.</p><ul><li>ə</li><li>Š</li><li>š</li><li>s</li></ul>",
+  "question-4":"Wie gehe ich beim Übersetzen vor?",
+  "answer-4": "<p>Bitte geben Sie die Region und im Optimalfall das Dorf/den Ort, in dem Sie aufgewachsen sind (oder dem Sie sich sprachlich am ehesten zuordnen) und Ihr Alter an. Damit bestimmen wir ihre dialektologische Herkunft. Falls Sie den gewünschten Ort nicht finden, vermerken Sie dies bitte in einem Kommentar.</p><p>Übersetzen Sie anschliessend die vorgegebenen Sätze in Ihren Dialekt. Es gibt dabei keine Vorschriften, wie Sie den Dialekt schreiben.</p>",
   "transcription-rules-heading": "Transkriptionregeln",
   "transcription-rule-1": "Ein vermuteter Wortlaut wird in eckigen Klammern [] geschrieben, z. B. [T]ue das mache...",
   "transcription-rule-2": "Wenn ein Wort oder ein Buchstabe nicht entziffert werden kann, wird folgender Platzhalter eingefügt: [...]",
   "transcription-rule-3": "Falls ein Wort nicht vollständig entziffert werden kann, kann folgender Platzhalter eingefügt werden: [unvollst.]",
   "transcription-rule-4": "Grundsatz: Die Transkription soll möglichst nahe am Original sein!",
-  "transcription-rule-5": "Alle Sonderzeichen sollten transkribiert werden! (Falls nicht möglich, siehe FAQs)."
+  "transcription-rule-5": "Alle Sonderzeichen sollten transkribiert werden!"
   },
   "en": {
   "heading": "FAQ",
-  "question-1": "What to do if you can't recognize a word/letter?",
-  "answer-1": "If a word or a letter can't be deciphered, the following placeholder is inserted: [...]<br><a href='#transkriptionskonventionen'>see transcription conventions</a>",
-  "question-2": "What to do if you can't transcribe a special character?",
-  "answer-2": "Look in the existing special character table. If the desired special character doesn't exist, please contact us via <a href='mailto:info@citizenscience.ch'>info@citizenscience.ch</a>.",
-  "question-3": "I come from X, I want to transcribe the sheet of Y, is that possible?",
-  "answer-3": "Basically, that's possible, yes. You can transcribe as many sheets as you like. Only the own translation of the 40 sentences can only be done once.",
+  "question-1": "What is the benefit of registering?",
+  "answer-1": "Registered users have the following benefits:<ul><li>Continue your work on a different computer</li><li> Access a history of sheets you have already transcribed (under your Profile)</li><li> Save your region and age range so you do not have to enter it each time</li></ul>",
+  "question-2": "How do I do the transcription?",
+  "answer-2": "First, you have 2 options: <ul> <br><li> Transcribe all sheets in a random order.</li><li>Transcribe sheets from a place within a canton.</li></ul><p> If you make no selection for your region, then you can go through all the sheets we have in our database, in a completely random order.</p><p>The image of the sheet is scrollable with your mouse (or touch screen) and you also have the ability to zoom in or out to make it clearer.</p><p> Beneath the image is a text box that starts the first sentence. You can move between sentences by clicking the Next button or by choosing a sentence number from the dropdown.</p><p> When you are completing a sheet, everything you do is saved locally on your machine. This means that you can move to Sentence 4, for example, move on to Sentence 5 and still go back to Sentence 4 to make changes. As soon as you submit your work, i.e. move on to the next sheet or end your session, then these transcriptions are saved in the database and can not be edited again. Each time you see the sheet again (to correct or complete it), your submission will be treated as new, although the input box will inform which sentences you already have submitted.</p><p> If you have any issues, you can use the comments section to discuss the sentence you are working on. Your comment is automatically assigned to the sentence. You will get help from the community and those involved in the project.</p>",
+  "question-3": "What about special characters?",
+  "answer-3": "<p>There may be some special characters that are hard to find on your keyboard. Some of these have been added above the input box for each sentence and can be clicked on to insert them into the end of the text you have written so far. So type in the desired letter first and then click on the special character to combine it with the previous letter.</p><p>For example: If you want to make the ā symbol, then you would type a and click the first button in the special characters list, so on ē. There are some special characters that you can insert directly at the desired position. These are the following symbols that you will find at the end of the special character series.</p><ul><li>ə</li><li>Š</li><li>š</li><li>s</li></ul>",
+  "question-4": "How do I translate?",
+  "answer-4": "<p>Please indicate the region and, ideally, the village/place where you grew up (or to which you are most likely to be linguistically assigned) and your age. This is how we determine your dialectological origin. If you cannot find the place you are looking for, please note this in a comment.</p><p>Then translate the sentences into your dialect. There are no rules about how you have to write the dialect.</p>",
   "transcription-rules-heading": "Transcription Rules",
   "transcription-rule-1": "A suspected wording is written in square brackets [], for example: [T]ue das mache...",
   "transcription-rule-2": "If a word or letter can't be deciphered, the following placeholder is inserted: [...]",
   "transcription-rule-3": "If a word can't be completely deciphered, the following wildcard can be inserted: [unvollst.]",
   "transcription-rule-4": "Principle: The transcription should be as close as possible to the original!",
-  "transcription-rule-5": "All special characters should be transcribed! (If not possible, see FAQs)."
+  "transcription-rule-5": "All special characters should be transcribed!"
   }
   }
 </i18n>
@@ -54,6 +58,9 @@
 
               <h3 class="subheading">{{ $t('question-3') }}</h3>
               <p v-html="$t('answer-3')"></p>
+
+              <h3 class="subheading">{{ $t('question-4') }}</h3>
+              <p v-html="$t('answer-4')"></p>
 
             </div>
             <div class="content-subsection">
