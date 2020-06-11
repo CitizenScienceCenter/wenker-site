091caeb2dd61789e6904ea95701a080d55be3fd8
Nikos G <ngeorgomanolis@gmail.com>
2020-06-11 08:05:57 +0200

    updated wenker locations #2

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ Vue.use(Croppa)
 Vue.use(Vuex)
 Vue.use(Meta)
 
-const v = '1.0.7'
+const v = '1.0.8'
 if (window.localStorage.getItem(('vuex'))) {
   const ls = JSON.parse(window.localStorage.getItem('vuex')).settings.version
   if (ls === undefined || ls !== v) {
